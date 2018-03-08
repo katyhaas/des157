60264b9083d216c448f289bb74fbc6c97fd5e6a9
Katy <kmhaas@ucdavis.edu>
2018-03-08 08:16:09 -0800

    yay

--- project/proto2/script.js
+++ project/proto2/script.js
@@ -18,80 +18,89 @@ var sect4 = document.querySelector('#sect4');
 var sect5 = document.querySelector('#sect5');
 
 //color button change variables
+
+//songs
 var song1 = document.querySelector("#song1");
 var song2 = document.querySelector("#song2");
 var song3 = document.querySelector("#song3");
 var song4 = document.querySelector("#song4");
 
+
+//color
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var color3 = document.querySelector("#color3");
 var color4 = document.querySelector("#color4");
 
+//patterns
 var pattern1 = document.querySelector("#patt1");
 var pattern2 = document.querySelector("#patt2");
 var pattern3 = document.querySelector("#patt3");
 var pattern4 = document.querySelector("#patt4");
 
-//pg reset
-refresh.addEventListener('click', function() {
-  sect1.style.display = 'block';
-  sect2.style.display = 'none';
-  sect3.style.display = 'none';
-  sect4.style.display = 'none';
-  sect5.style.display = 'none';
-
-  //button colors back to normal
-  song1.style.backgroundColor = "rgb(165, 153, 232)";
-  song1.style.opacity = "1";
-  song2.style.backgroundColor = "rgb(165, 153, 232)";
-  song2.style.opacity = "1";
-  song3.style.backgroundColor = "rgb(165, 153, 232)";
-  song3.style.opacity = "1";
-  song4.style.backgroundColor = "rgb(165, 153, 232)";
-  song4.style.opacity = "1";
-
-  color1.style.backgroundColor = "rgb(165, 153, 232)";
-  color1.style.opacity = "1";
-  color2.style.backgroundColor = "rgb(165, 153, 232)";
-  color2.style.opacity = "1";
-  color3.style.backgroundColor = "rgb(165, 153, 232)";
-  color3.style.opacity = "1";
-  color4.style.backgroundColor = "rgb(165, 153, 232)";
-  color4.style.opacity = "1";
+var texture1 = false;
+var texture2 = false;
+var texture3 = false;
+var texture4 = false;
 
 
-  pattern1.style.backgroundColor = "rgb(165, 153, 232)";
-  pattern1.style.opacity = "1";
-  pattern2.style.backgroundColor = "rgb(165, 153, 232)";
-  pattern2.style.opacity = "1";
-  pattern3.style.backgroundColor = "rgb(165, 153, 232)";
-  pattern3.style.opacity = "1";
-  pattern4.style.backgroundColor = "rgb(165, 153, 232)";
-  pattern4.style.opacity = "1";
+// animate patterns
+var shSize = 0;
+var pulse;
+var song;
+var amp;
 
-})
+var circles = [];
+var triangles = [];
+var lines = [];
+var squares = [];
 
-//navigation buttons display
-start.addEventListener('click', function() {
-  sect1.style.display = 'none';
-  sect2.style.display = 'block';
-});
 
-songz.addEventListener('click', function() {
-  sect2.style.display = 'none';
-  sect3.style.display = 'block';
-})
 
-colorz.addEventListener('click', function() {
-  sect3.style.display = 'none';
-  sect4.style.display = 'block';
-})
+//song
+function preload() {
+  song = loadSound("https://katyhaas.github.io/des157/studio4/music/song1_Q.mp3");
+}
 
-patternz.addEventListener('click', function() {
-  sect4.style.display = 'none';
-  sect5.style.display = 'block';
-})
+//make canvas
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noFill();
+  stroke('#2B4774');
+  strokeWeight(3);
+
+  amp = new p5.Amplitude();
+  amp.setInput(song);
+
+  for (var i = 0; i < 20; i++) {
+    circles.push(new patt1());
+    triangles.push(new patt4());
+    lines.push(new patt3());
+    squares.push(new patt2());
+    }
+}
+
+function draw() {
+  pulse = amp.getLevel();
+  shSize = map(pulse, 0, 0.3, 10, 200);
+  if (texture1 == true) {
+    for (var i = 0; i < circles.length; i++) {
+      circles[i].display();
+    }
+  } else if (texture2 == true) {
+    for (var i = 0; i < squares.length; i++) {
+      triangles[i].display();
+    }
+  } else if (texture3 == true) {
+    for (var i = 0; i < lines.length; i++) {
+      lines[i].display();
+    }
+  } else if (texture4 == true) {
+    for (var i = 0; i < triangles.length; i++) {
+      squares[i].display();
+    }
+  }
+}
 
 //single button selected + highlighted
 
@@ -146,6 +155,7 @@ color1.addEventListener('click', function() {
   color3.style.opacity = "0.6";
   color4.style.backgroundColor = "rgb(165, 153, 232)";
   color4.style.opacity = "0.6";
+
 });
 
 color2.addEventListener('click', function() {
@@ -156,6 +166,7 @@ color2.addEventListener('click', function() {
   color3.style.opacity = "0.6";
   color4.style.backgroundColor = "rgb(165, 153, 232)";
   color4.style.opacity = "0.6";
+
 });
 
 color3.addEventListener('click', function() {
@@ -166,6 +177,7 @@ color3.addEventListener('click', function() {
   color3.style.backgroundColor = "rgb(153, 182, 232)";
   color4.style.backgroundColor = "rgb(165, 153, 232)";
   color4.style.opacity = "0.6";
+
 });
 
 color4.addEventListener('click', function() {
@@ -176,6 +188,7 @@ color4.addEventListener('click', function() {
   color3.style.backgroundColor = "rgb(165, 153, 232)";
   color3.style.opacity = "0.6";
   color4.style.backgroundColor = "rgb(153, 182, 232)";
+
 });
 
 
@@ -188,6 +201,11 @@ pattern1.addEventListener('click', function() {
   pattern3.style.opacity = "0.6";
   pattern4.style.backgroundColor = "rgb(165, 153, 232)";
   pattern4.style.opacity = "0.6";
+
+  texture1 = true;
+  texture2 = false;
+  texture3 = false;
+  texture4 = false;
 });
 
 pattern2.addEventListener('click', function() {
@@ -198,6 +216,11 @@ pattern2.addEventListener('click', function() {
   pattern3.style.opacity = "0.6";
   pattern4.style.backgroundColor = "rgb(165, 153, 232)";
   pattern4.style.opacity = "0.6";
+
+  texture1 = false;
+  texture2 = true;
+  texture3 = false;
+  texture4 = false;
 });
 
 pattern3.addEventListener('click', function() {
@@ -209,6 +232,11 @@ pattern3.addEventListener('click', function() {
   pattern4.style.backgroundColor = "rgb(165, 153, 232)";
   pattern4.style.opacity = "0.6";
 
+  texture1 = false;
+  texture2 = false;
+  texture3 = true;
+  texture4 = false;
+
 });
 
 pattern4.addEventListener('click', function() {
@@ -219,4 +247,133 @@ pattern4.addEventListener('click', function() {
   pattern3.style.backgroundColor = "rgb(165, 153, 232)";
   pattern3.style.opacity = "0.6";
   pattern4.style.backgroundColor = "rgb(153, 182, 232)";
+
+  texture1 = false;
+  texture2 = false;
+  texture3 = false;
+  texture4 = true;
 });
+
+
+
+//pg reset
+refresh.addEventListener('click', function() {
+  sect1.style.display = 'block';
+  sect2.style.display = 'none';
+  sect3.style.display = 'none';
+  sect4.style.display = 'none';
+  sect5.style.display = 'none';
+
+  //button colors back to normal
+  song1.style.backgroundColor = "rgb(165, 153, 232)";
+  song1.style.opacity = "1";
+  song2.style.backgroundColor = "rgb(165, 153, 232)";
+  song2.style.opacity = "1";
+  song3.style.backgroundColor = "rgb(165, 153, 232)";
+  song3.style.opacity = "1";
+  song4.style.backgroundColor = "rgb(165, 153, 232)";
+  song4.style.opacity = "1";
+
+  color1.style.backgroundColor = "rgb(165, 153, 232)";
+  color1.style.opacity = "1";
+  color2.style.backgroundColor = "rgb(165, 153, 232)";
+  color2.style.opacity = "1";
+  color3.style.backgroundColor = "rgb(165, 153, 232)";
+  color3.style.opacity = "1";
+  color4.style.backgroundColor = "rgb(165, 153, 232)";
+  color4.style.opacity = "1";
+
+
+  pattern1.style.backgroundColor = "rgb(165, 153, 232)";
+  pattern1.style.opacity = "1";
+  pattern2.style.backgroundColor = "rgb(165, 153, 232)";
+  pattern2.style.opacity = "1";
+  pattern3.style.backgroundColor = "rgb(165, 153, 232)";
+  pattern3.style.opacity = "1";
+  pattern4.style.backgroundColor = "rgb(165, 153, 232)";
+  pattern4.style.opacity = "1";
+
+  texture1 = false;
+  texture2 = false;
+  texture3 = false;
+  texture4 = false;
+
+  clear();
+  song.stop();
+});
+
+//navigation buttons display
+start.addEventListener('click', function() {
+  sect1.style.display = 'none';
+  sect2.style.display = 'block';
+});
+
+songz.addEventListener('click', function() {
+  sect2.style.display = 'none';
+  sect3.style.display = 'block';
+});
+
+colorz.addEventListener('click', function() {
+  sect3.style.display = 'none';
+  sect4.style.display = 'block';
+});
+
+patternz.addEventListener('click', function() {
+  sect4.style.display = 'none';
+  sect5.style.display = 'block';
+
+  song.play();
+});
+
+
+
+// circles
+function patt1() {
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(5, 30);
+
+  this.display = function() {
+    fill('rgba(238,119,82)');
+    stroke('#2B4774');
+    strokeWeight(2);
+    ellipse(this.x, this.y, this.diameter + shSize, this.diameter + shSize);
+  };
+}
+
+// other option
+function patt2() {
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(5, 30);
+
+  this.display = function() {
+    fill('rgba(255,255,255)');
+    rect(this.x, this.y, this.diameter + shSize, this.diameter + shSize);
+  };
+}
+
+// linez
+function patt3() {
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(6, height);
+
+  this.display = function() {
+    stroke('rgb(255,255,255)');
+    strokeWeight(2);
+    line(this.x, this.y, this.diameter + shSize, this.diameter + shSize);
+  };
+}
+
+// squarez
+function patt4() {
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(5, 30);
+
+  this.display = function() {
+    fill('rgba(255,255,255)');
+    rect(this.x, this.y, this.diameter + shSize, this.diameter + shSize);
+  };
+}
